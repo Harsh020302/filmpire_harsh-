ccbb8a559e545e47bacb58732f69e5f655a6085f
Harsha <harsha@123.com>
2022-10-20 20:14:59 +0530

    file and folder structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
+import { CssBaseline} from '@mui/material';
+import { Route , Routes } from 'react-router-dom';
+import { ThemeProvider } from '@mui/styles';
+import useStyles from './styles';
+
+import { Actors , MovieInformation , Movies , NavBar , Profile } from './';
 
 const App = () => {
-  return (
-    <div>
-        App
+  const classes = useStyles();
+  return(
+    <div className={classes.root}>
+      <CssBaseline/>
+      <NavBar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Routes>
+          <Route exact path='/' element={<MovieInformation/>}/>
+          <Route exact path='/movies/:id' element={<Movies/>} />
+          <Route exact path='/actors/:id' element={<Actors/>} />
+          <Route exact path='/profile/:id' element={<Profile/>} />
+        </Routes>
+      </main>
+
     </div>
-  );
+  )
+  
 }
 
 export default App;
---
+++ src/components/styles.js
@@ -0,0 +1,12 @@
+import { makeStyles} from '@mui/styles';
+
+export default makeStyles(() => ({
+    root : {
+        display : 'flex',
+        height : '100px',
+    },
+    toolbar : {
+        flexGrow: '1',
+        padding: '2em',
+    }
+}));
